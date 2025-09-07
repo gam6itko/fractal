76f256e82562c9d90608944233603a3e621267d8
gam6itko <gam6itko@bestchange.dev>
2025-09-08 01:03:51 +0300

    wf

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,6 +5,7 @@ $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__);
 
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
         '@PHP82Migration' => true,
